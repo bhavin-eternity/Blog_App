3b0d40093775095ce615e4a7cc302c1e1c78d3d0
Mr Robot <robot@example.com>
2026-07-02 18:08:01 +0530

    feat: add signup and login with JWT auth

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const userScheme = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -17,18 +17,18 @@ const userScheme = new mongoose.Schema({
     },
 }, { timestamps: true });
 
-userScheme.pre('save', async function (next){
+userSchema.pre('save', async function (next){
     if(!this.isModified('password')){
-        return next()
+        return;
     }
 
     const salt =await bcrypt.genSalt(10);
     this.password =await bcrypt.hash(this.password,salt);
-    next()
+    
 });
 
-userScheme.methods.matchPassword =async function (enteredPassword) {
+userSchema.methods.matchPassword =async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword,this.password);
 };
 
-module.exports =mongoose.model('User',userScheme);
+module.exports =mongoose.model('User',userSchema); 
--- server/src/server.js
+++ server/src/server.js
@@ -1,16 +1,25 @@
 require('dotenv').config()
 
 const express = require('express')
-const connectDB =require('./config/db');
+const connectDB = require('./config/db');
+const cors = require('cors');
+const authRoutes = require('./routes/authRoutes');
 const app = express()
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
     res.send('Server is running')
 });
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
